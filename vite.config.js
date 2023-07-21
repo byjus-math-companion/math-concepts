84b25cb29d672ea8f64994b04cf1bc20093322fd
Garvit <garvit.joshi@byjus.com>
2023-07-21 22:40:04 +0530

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://byjus-math-companion.github.io/maths-concepts/",
+  base: "/maths-concepts/",
 })
